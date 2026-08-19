598ef1b583c0fba820d8d8b00a99e00f52783464
mars <69923391+Algrythm@users.noreply.github.com>
2026-08-19 17:01:09 -0600

    Update signIn.f

--- build/melBase/signIn.f
+++ build/melBase/signIn.f
@@ -4,12 +4,14 @@ console::push("Welcome to sign in!");
 console::write("Please enter your account name: ");
 .i<enterName>;
 
+#- check name
 ifn(i<enterName> == i<username>)
 BEGINIF{
     console::push("That account doesn't exist!");
     terminate(); 
 }ENDIF
 
+#- check pass
 console::write("Please enter your account password: ");
 .i<enterPass>;
 ifn(i<enterPass> == i<password>)
@@ -18,5 +20,6 @@ BEGINIF{
     terminate();
 }ENDIF
 
+#- if all checks pass, print
 console::write("Welcome to your account, ");
 console::push(i<username>);
