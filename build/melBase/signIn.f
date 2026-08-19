e2062a012e85e0086f63ae4df7522c74c43c1cab
mars <69923391+Algrythm@users.noreply.github.com>
2026-08-19 16:19:53 -0600

    mel code

---
+++ build/melBase/signIn.f
@@ -0,0 +1,22 @@
+#- expected args: username, password
+
+console::push("Welcome to sign in!");
+console::write("Please enter your account name: ");
+.i<enterName>;
+
+ifn(i<enterName> == i<username>)
+BEGINIF{
+    console::push("That account doesn't exist!");
+    terminate(); 
+}ENDIF
+
+console::write("Please enter your account password: ");
+.i<enterPass>;
+ifn(i<enterPass> == i<password>)
+BEGINIF{
+    console::push("Incorect password!");
+    terminate();
+}ENDIF
+
+console::write("Welcome to your account, ");
+console::push(i<username>);
