2ba61b30a8c10b04e3b14171e9cff3ab2616829e
mars <69923391+Algrythm@users.noreply.github.com>
2026-08-22 18:07:59 -0600

    Update extension version in installer script

--- installer/install_vscode_extension.bat
+++ installer/install_vscode_extension.bat
@@ -5,4 +5,4 @@ echo Welcome to the MEL VSCode Extension installer.
 echo This will install the MEL Vsix.
 pause
 cls
-code --install-extension mel-0.0.2.vsix
+code --install-extension mel-0.0.3.vsix
